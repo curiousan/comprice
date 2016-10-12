97151c6f1b39f4b581de279c6b49d823c845316e
sushil <suzilxptr@gmail.com>
2016-10-12 13:36:59 +0300

    Update mapController.js

--- comprice_client/shopDetails/mapController.js
+++ comprice_client/shopDetails/mapController.js
@@ -16,7 +16,7 @@ function mapController($scope,productService,queryProduct){
                 latitude=codr[0];
                 longitude=codr[1];
                 console.log("The data"+ latitude+ " "+ longitude);
-                    $('body').append('<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBvRnqDJdIoDSfHGmyRakdUR2TRQ4j1d2w&callback=initMap" async defer></script>')
+                    $('body').append('<script src="https://maps.googleapis.com/maps/api/js?key=*********&callback=initMap" async defer></script>')
 
 
             })
